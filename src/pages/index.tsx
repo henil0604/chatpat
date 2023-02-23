33115279dc03fade6f97ba0bd69d67b2cceda2a4
Henil Malaviya <henilmalaviya06@gmail.com>
2023-02-23 09:53:38 +0530

    using mongodb & tag of expected date of release

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
       </Head>
       <BaseLayout>
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#4DABF7] to-[#1864AB]">
-          <Paper shadow="xl" color="red" p="md">
+          <Paper shadow="xl" p="md">
             Coming Soon
           </Paper>
           <div className="container flex flex-col items-center justify-center gap-2 px-4 py-16 ">
@@ -24,6 +24,10 @@ const Home: NextPage = () => {
             </h1>
             <p className="italic text-white">Your Private Place to talk</p>
           </div>
+          <Paper shadow="xl" my="sm" p="xs">
+            Expected Date of Release:{" "}
+            <span className="text-red-700">1 May 2023</span>
+          </Paper>
         </main>
       </BaseLayout>
     </>
