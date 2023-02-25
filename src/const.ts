a2672c3c9b5e31645cd246831cb59e85314f3947
Henil Malaviya <henilmalaviya06@gmail.com>
2023-02-25 19:45:55 +0530

    change tagline

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "ChatPat";
 
-export const APP_TAGLINE = "Your Private Place to talk";
+export const APP_TAGLINE = "Flavour of Privacy";
 
 export const APP_EXPECTED_RELEASE_DATE = "1 May 2023";
