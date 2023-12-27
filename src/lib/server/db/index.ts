23c0e694e25b310e5431f76ebb02e57073a21471
Henil Malaviya <henilmalaviya06@gmail.com>
2023-12-27 22:38:38 +0530

    chore: guaranteed db connection before accepting requests & better logging

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,28 +3,57 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import { DATABASE_URL } from '$env/static/private';
-import { LogType, logger } from '../modules/log';
+import { LogType, logger } from '$lib/server/modules/log';
+
+const log = logger().prefix("db");
 
 export const pool = new pg.Pool({
-	connectionString: DATABASE_URL
+	connectionString: DATABASE_URL,
+	log: (...messages) => {
+		// internal pool logs
+		log.clone()
+			.type(LogType.UNKNOWN)
+			.prefix("internal")
+			.message(...messages)
+			.commit();
+	},
 });
 
-pool.on('connect', () => {
-	logger()
-		.type(LogType.SUCCESS)
-		.prefix("db")
-		.message("connected")
-		.commit()
-})
+// connect to database
+function connect() {
+	return new Promise(async resolve => {
 
-pool.on('error', () => {
-	logger()
-		.type(LogType.ERROR)
-		.prefix("db")
-		.message("error")
-		.commit();
-})
+		function onConnectionError() {
+			log
+				.type(LogType.ERROR)
+				.message("connection error")
+				.commit();
 
-await pool.connect();
+			log
+				.type(LogType.OK)
+				.message("retry in 3 seconds")
+				.commit();
+
+			// retry after n seconds
+			setTimeout(async () => {
+				resolve(await connect());
+			}, 3 * 1000);
+		}
+
+		await pool.connect().then(() => {
+			// if connection is successful, resolve the promise
+
+			log
+				.type(LogType.SUCCESS)
+				.message("connected")
+				.commit()
+
+			resolve(void 0);
+		}).catch(onConnectionError);
+	})
+}
+
+// wait for the connection
+await connect()
 
 export const db = drizzle(pool);
