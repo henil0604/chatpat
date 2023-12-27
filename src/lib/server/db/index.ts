15e49b9ca5a11ded35a7db5581585718345800b0
Henil Malaviya <henilmalaviya06@gmail.com>
2023-12-27 20:55:06 +0530

    chore: added log for successful database connection

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,12 +3,18 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import { DATABASE_URL } from '$env/static/private';
+import { LogType, logger } from '../modules/log';
 
 export const pool = new pg.Pool({
 	connectionString: DATABASE_URL
 });
 
 await pool.connect().then(() => {
-	console.log('Connected to database');
+	logger()
+		.type(LogType.SUCCESS)
+		.prefix("db")
+		.message("connected")
+		.commit()
 });
+
 export const db = drizzle(pool);
