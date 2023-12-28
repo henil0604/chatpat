875ffc9e9d91f29ae45044683e2229dcf07ed0a4
Henil Malaviya <henilmalaviya06@gmail.com>
2023-12-28 21:01:42 +0530

    add trpc handler in hook sequence

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,9 @@
 import { luciaHandler } from '$lib/server/lucia/handler';
+import trpcHandle from '$lib/server/trpc/handler';
 import { LogType, logger } from '$lib/server/modules/log';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = sequence(luciaHandler, async ({ event, resolve }) => {
+export const handle = sequence(luciaHandler, trpcHandle, async ({ event, resolve }) => {
 
     logger()
         .type(LogType.OK)
