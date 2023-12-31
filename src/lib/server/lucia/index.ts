c414f65b2d98f7015c4f6747ded0e02c41e9f645
Henil Malaviya <henilmalaviya06@gmail.com>
2023-12-31 17:47:55 +0530

    add google provider

--- src/lib/server/lucia/index.ts
+++ src/lib/server/lucia/index.ts
@@ -5,8 +5,9 @@ import { pg } from '@lucia-auth/adapter-postgresql';
 import { pool } from '$lib/server/db';
 import { dev } from '$app/environment';
 import { sveltekit } from 'lucia/middleware';
-import { AUTH_GITHUB_CLIENT_ID, AUTH_GITHUB_CLIENT_SECRET } from '$env/static/private';
-import { github } from '@lucia-auth/oauth/providers';
+import { AUTH_GITHUB_CLIENT_ID, AUTH_GITHUB_CLIENT_SECRET, AUTH_GOOGLE_CLIENT_ID, AUTH_GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { github, google } from '@lucia-auth/oauth/providers';
+import { getLoginURL } from '$lib/modules/auth';
 
 export const auth = lucia({
 	env: dev ? 'DEV' : 'PROD',
@@ -27,5 +28,11 @@ export const githubAuth = github(auth, {
 	clientId: AUTH_GITHUB_CLIENT_ID,
 	clientSecret: AUTH_GITHUB_CLIENT_SECRET
 });
+
+export const googleAuth = google(auth, {
+	clientId: AUTH_GOOGLE_CLIENT_ID,
+	clientSecret: AUTH_GOOGLE_CLIENT_SECRET,
+	redirectUri: process.env.VERCEL_URL ? `${process.env.VERCEL_URL}/auth/google/callback` : 'http://localhost:5173/auth/google/callback'
+})
 
 export type Auth = typeof auth;
