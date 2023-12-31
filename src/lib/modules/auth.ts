b9afc311ba971be433562f85407729f1123de3ee
Henil Malaviya <henilmalaviya06@gmail.com>
2023-12-31 17:47:21 +0530

    refactor: extract `getLoginURL` function for reusability

--- src/lib/modules/auth.ts
+++ src/lib/modules/auth.ts
@@ -2,10 +2,14 @@ import { AUTH_ENABLED_METHODS, AUTH_OAUTH_BASE_URL } from '$lib/const';
 
 type Method = (typeof AUTH_ENABLED_METHODS)[number];
 
-export function login(method: Method, redirectURL?: string) {
-	let url = new URL(`${location.origin}${AUTH_OAUTH_BASE_URL}/${method}`);
+export function getLoginURL(method: Method, redirectURL?: string, origin?: string) {
+	let url = new URL(`${origin || location.origin}${AUTH_OAUTH_BASE_URL}/${method}`);
 	if (redirectURL) {
 		url.searchParams.set('redirectURL', encodeURIComponent(redirectURL));
 	}
-	location.href = url.toString();
+	return url.toString();
+}
+
+export function login(method: Method, redirectURL?: string) {
+	location.href = getLoginURL(method, redirectURL);
 }
