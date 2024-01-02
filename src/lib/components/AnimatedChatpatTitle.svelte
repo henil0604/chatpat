8987cdc9e998e3bc26be77bfa3df645434c68871
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-02 23:33:15 +0530

    better blob animation

--- src/lib/components/AnimatedChatpatTitle.svelte
+++ src/lib/components/AnimatedChatpatTitle.svelte
@@ -1,21 +1,38 @@
 <script lang="ts">
 	import AnimatedFloatingBlob from '$lib/components/AnimatedFloatingBlob.svelte';
 	import { cn } from '$lib/utils';
+	import { onMount } from 'svelte';
 
 	export let size = 30;
 	let className = '';
 	export { className as class };
+
+	const DEFAULT_ANIMATION_SPEED = 0.003;
+	let animationSpeed = DEFAULT_ANIMATION_SPEED;
+	let wrapperRef: HTMLDivElement;
+
+	onMount(() => {
+		wrapperRef.addEventListener('mouseover', () => {
+			animationSpeed = 0.01;
+		});
+		wrapperRef.addEventListener('mouseleave', () => {
+			animationSpeed = DEFAULT_ANIMATION_SPEED;
+		});
+	});
 </script>
 
+<!-- <AnimatedFloatingBlob class="h-[100px] w-[100px]" /> -->
 <div
 	class={cn('flex gap-2 overflow-visible font-extrabold', className)}
 	style="font-size: {size | 0}px;"
+	bind:this={wrapperRef}
 >
 	<div>Chat</div>
-	<div class="group relative overflow-visible text-white">
-		<div class="absolute left-0 top-0 z-[-1] h-full w-full">
+	<div class="group relative max-h-fit max-w-fit overflow-visible text-white">
+		<div class="absolute left-0 top-0 z-[-1] h-full w-full overflow-visible">
 			<AnimatedFloatingBlob
-				class="scale-x-[1.3] scale-y-[1.1] bg-black transition duration-[300ms]"
+				noiseStep={animationSpeed}
+				class="scale-x-[1.1] scale-y-[0.9] transition duration-[300ms]"
 			/>
 		</div>
 		<span>Pat</span>
--- src/lib/components/AnimatedFloatingBlob.svelte
+++ src/lib/components/AnimatedFloatingBlob.svelte
@@ -1,52 +1,100 @@
 <script lang="ts">
-	import { cn } from '$lib/utils';
+	// modified code from:
+	// LINK: https://codepen.io/georgedoescode/pen/oNzamjV
 
-	export let width: number = 0;
-	export let height: number = 0;
+	import { cn } from '$lib/utils';
+	import { onMount } from 'svelte';
+	import SimplexNoise from 'simplex-noise';
+	import { spline } from '$lib/utils/spline';
+
 	let className = '';
 	export { className as class };
+
+	let pathEl: SVGPathElement;
+
+	export let noiseStep = 0.002;
+
+	const simplex = new SimplexNoise();
+
+	function map(n: number, start1: number, end1: number, start2: number, end2: number) {
+		return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+	}
+
+	function noise(x: number, y: number) {
+		return simplex.noise2D(x, y);
+	}
+
+	function createPoints() {
+		const points = [];
+		// how many points do we need
+		const numPoints = 9;
+		// used to equally space each point around the circle
+		const angleStep = (Math.PI * 2) / numPoints;
+		// the radius of the circle
+		const rad = 120;
+
+		for (let i = 1; i <= numPoints; i++) {
+			// x & y coordinates of the current point
+			const theta = i * angleStep;
+
+			const x = 100 + Math.cos(theta) * rad;
+			const y = 100 + Math.sin(theta) * rad;
+
+			// store the point's position
+			points.push({
+				x: x,
+				y: y,
+				// we need to keep a reference to the point's original point for when we modulate the values later
+				originX: x,
+				originY: y,
+				// more on this in a moment!
+				noiseOffsetX: Math.random() * 1000,
+				noiseOffsetY: Math.random() * 1000
+			});
+		}
+
+		return points;
+	}
+
+	onMount(() => {
+		const points = createPoints();
+
+		(function animate() {
+			pathEl.setAttribute('d', spline(points, 1, true));
+
+			// for every point...
+			for (let i = 0; i < points.length; i++) {
+				const point = points[i];
+
+				// return a pseudo random value between -1 / 1 based on this point's current x, y positions in "time"
+				const nX = noise(point.noiseOffsetX, point.noiseOffsetX);
+				const nY = noise(point.noiseOffsetY, point.noiseOffsetY);
+				// map this noise value to a new value, somewhere between it's original location -20 and it's original location + 20
+				const x = map(nX, -1, 1, point.originX - 20, point.originX + 20);
+				const y = map(nY, -1, 1, point.originY - 20, point.originY + 20);
+
+				// update the point's current coordinates
+				point.x = x;
+				point.y = y;
+
+				// progress the point's x, y values through "time"
+				point.noiseOffsetX += noiseStep;
+				point.noiseOffsetY += noiseStep;
+			}
+
+			requestAnimationFrame(animate);
+		})();
+	});
 </script>
 
-<div
-	class={cn('blob ', className)}
-	style="width: {width === 0 ? '100%' : `${width}px`}; height: {height === 0
-		? '100%'
-		: `${height}px`};"
-></div>
-
-<style scoped>
-	.blob {
-		animation: animate 10s ease infinite alternate;
-		transition: all 1s ease-in-out;
-	}
-
-	@keyframes animate {
-		0% {
-			border-radius: 46% 55% 77% 44% / 50% 31% 76% 63%;
-		}
-		15% {
-			border-radius: 46% 55% 56% 64% / 50% 31% 76% 63%;
-		}
-		25% {
-			border-radius: 34% 74% 56% 62% / 50% 24% 76% 63%;
-		}
-		35% {
-			border-radius: 34% 74% 56% 62% / 73% 24% 76% 36%;
-		}
-		50% {
-			border-radius: 34% 74% 33% 73% / 73% 43% 76% 38%;
-		}
-		60% {
-			border-radius: 70% 63% 33% 73% / 73% 43% 61% 38%;
-		}
-		75% {
-			border-radius: 70% 63% 60% 69% / 48% 43% 61% 54%;
-		}
-		85% {
-			border-radius: 30% 63% 60% 69% / 48% 43% 61% 54%;
-		}
-		100% {
-			border-radius: 46% 55% 77% 44% / 50% 31% 76% 63%;
-		}
-	}
-</style>
+<div class={cn('max-h-full max-w-full overflow-visible', className)}>
+	<svg class="overflow-visible" viewBox="0 0 200 200">
+		<defs>
+			<linearGradient id="gradient" gradientTransform="rotate(90)">
+				<stop id="gradientStop1" offset="0%" stop-color="#000" />
+				<stop id="gradientStop2 " offset="100%" stop-color="#333" />
+			</linearGradient>
+		</defs>
+		<path d="" bind:this={pathEl} fill="url('#gradient')"></path>
+	</svg>
+</div>
