ac3a681094ebdb246119a23facc89a6cb8fa570b
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-02 18:42:35 +0530

    fix: replace `info` with `getInfo()` due to changes in `log` module API

--- src/lib/server/modules/log/log.test.ts
+++ src/lib/server/modules/log/log.test.ts
@@ -4,21 +4,21 @@ import { LogType, logger } from './index';
 describe('log module', () => {
 	it('simple hello world log', () => {
 		const log = logger().type(LogType.OK);
-		expect(log.info.type).toBe(LogType.OK);
+		expect(log.getInfo().type).toBe(LogType.OK);
 		log.message('Hello World');
-		expect(log.info.message[0]).toBe('Hello World');
+		expect(log.getInfo().message[0]).toBe('Hello World');
 	});
 
 	it('with prefix', () => {
 		const log = logger().type(LogType.OK).message('Hello World');
 		log.prefix('server');
-		expect(log.info.prefixes[0]).toBe('server');
+		expect(log.getInfo().prefixes[0]).toBe('server');
 	});
 
 	it('with callback', () => {
 		const log = logger().type(LogType.OK).message('Hello World');
-		log.on('commit', () => {});
-		expect(log.info.listeners[0].event).toBe('commit');
-		expect(log.info.listeners[1]).toBeUndefined();
+		log.on('commit', () => { });
+		expect(log.getInfo().listeners[0].event).toBe('commit');
+		expect(log.getInfo().listeners[1]).toBeUndefined();
 	});
 });
