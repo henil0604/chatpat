937b4c35863051345d9af08abfc1d2bc68331c7a
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-02 13:16:45 +0530

    replace lucia handler with auth.js handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,15 @@
-import { luciaHandler } from '$lib/server/lucia/handler';
+import { authHandler } from '$lib/server/auth';
 import trpcHandle from '$lib/server/trpc/handler';
 import { LogType, logger } from '$lib/server/modules/log';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = sequence(luciaHandler, trpcHandle, async ({ event, resolve }) => {
+export const handle = sequence(async ({ event, resolve }) => {
 	logger()
 		.type(LogType.OK)
 		.prefix('hook')
 		.prefix(event.request.method.toLowerCase())
-		.message(event.url.pathname)
+		.message(event.url.pathname + event.url.search)
 		.commit();
 
 	return resolve(event);
-});
+}, authHandler, trpcHandle);
