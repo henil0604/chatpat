bd24aef57bd8b5852b7c8a91b513540d63034a01
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-02 19:06:04 +0530

    add social links and email

--- src/lib/const/index.ts
+++ src/lib/const/index.ts
@@ -2,3 +2,8 @@ export const AUTH_BASE_URL = '/auth';
 export const AUTH_OAUTH_BASE_URL = `${AUTH_BASE_URL}`;
 
 export const AUTH_ENABLED_METHODS = ['github', 'google'] as const;
+
+export const SOCIAL_TWITTER_HANDLE = 'TeamChatpat';
+export const SOCIAL_TWITTER_LINK = `https://twitter.com/${SOCIAL_TWITTER_HANDLE}`;
+
+export const SOCIAL_EMAIL = "chatpat@henil.xyz"
