07063f47344e38c1211245e3a531a1fd3969c77a
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-27 01:55:30 +0530

    add callback functions and event handlers

--- src/lib/server/auth/index.ts
+++ src/lib/server/auth/index.ts
@@ -9,6 +9,8 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 import Google from '@auth/sveltekit/providers/google';
 import { db } from '$lib/server/db';
+import type { Session } from '@auth/core/types';
+import type { AdapterUser } from '@auth/core/adapters';
 
 export const authHandler = SvelteKitAuth({
 	providers: [
@@ -24,6 +26,36 @@ export const authHandler = SvelteKitAuth({
 		})
 	],
 	debug: true,
+	// @ts-ignore
 	adapter: PrismaAdapter(db),
-	trustHost: true
+	trustHost: true,
+	callbacks: {
+		session: async (params) => {
+			const { session, user } = params as {
+				session: Session,
+				user: AdapterUser
+			};
+
+			if (session.user) {
+				session.user.id = user.id;
+				session.user.hasCompletedProfile = user.hasCompletedProfile;
+				session.user.username = user.username;
+			}
+
+			return session;
+		},
+	},
+	events: {
+		// triggered when new users is created
+		async createUser({ user }) {
+			await db.user.update({
+				where: {
+					id: user.id
+				},
+				data: {
+					hasCompletedProfile: false,
+				}
+			})
+		}
+	}
 });
