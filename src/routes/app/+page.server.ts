4e6dd841d4b8c8d9489043c8d2f359d07ae650a2
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-27 01:56:18 +0530

    if user has incomplete profile, redirect them to onboarding page

---
+++ src/routes/app/+page.server.ts
@@ -0,0 +1,14 @@
+import { Pages } from "$lib/const";
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+
+    const { url, params, parent } = event;
+
+    const { session } = await parent();
+
+    if (session?.user.hasCompletedProfile === false) {
+        throw redirect(301, Pages.OnBoarding.path);
+    }
+};
