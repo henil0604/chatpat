c6d56e87808eb778e5e32d8670ad44c075c25212
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-27 01:54:57 +0530

    add `username` and `hasCompletedProfile` fields

--- src/app.d.ts
+++ src/app.d.ts
@@ -14,14 +14,18 @@ declare global {
 export declare module '@auth/core/types' {
 	// User coming from database
 	interface User {
-		id?: string;
+		id: string;
+		username?: string;
+		hasCompletedProfile: boolean
 	}
 	interface Session {
 		// user that will be set to session
 		user: {
-			id?: string;
+			id: string;
+			username?: string;
+			hasCompletedProfile: boolean
 		} & DefaultSession['user'];
 	}
 }
 
-export {};
+export { };
