3795c3b2ba162a4c9abf62ba740a727b7249b355
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-28 00:44:08 +0530

    Simple Global Loading

---
+++ src/lib/components/GlobalLoading.svelte
@@ -0,0 +1,7 @@
+<script>
+	import AnimatedChatpatTitle from '$lib/components/AnimatedChatpatTitle.svelte';
+</script>
+
+<div class="flex-center absolute left-0 top-0 z-[100] h-full w-full bg-background text-foreground">
+	<AnimatedChatpatTitle class="gap-4" size={40} />
+</div>
