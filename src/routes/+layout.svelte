fa523e7836ec3dfb21aaa44e0d7b7a4077f7151b
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-28 00:46:38 +0530

    add ModeWatcher and Toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,17 @@
 	import 'tippy.js/dist/tippy.css';
 	import 'tippy.js/animations/scale.css';
 	import 'tippy.js/animations/perspective.css';
+	import { loading } from '$lib/store/global';
+	import GlobalLoading from '$lib/components/GlobalLoading.svelte';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { ModeWatcher, mode } from 'mode-watcher';
 </script>
+
+{#if $loading === true}
+	<GlobalLoading />
+{/if}
+
+<ModeWatcher defaultMode={'light'} />
+<Toaster richColors theme={$mode} />
 
 <slot />
