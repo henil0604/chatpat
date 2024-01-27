6eba73f5bcb8c2f91774480111ee5f7da5209353
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-28 00:46:54 +0530

    remove unused variables

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	const { url, params, parent } = event;
+	const { parent } = event;
 
 	const { session } = await parent();
 
