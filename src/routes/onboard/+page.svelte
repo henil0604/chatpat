12f3c38d5f7e4b9e6f265a7da3aced9585e84923
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-27 19:11:46 +0530

    username checking working on frontend

--- src/routes/onboard/+page.svelte
+++ src/routes/onboard/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { Dices, Info } from 'lucide-svelte';
+	import { Dices, Info, Triangle, AlertTriangle, AtSign, BadgeCheck } from 'lucide-svelte';
 	import { createAvatar } from '@dicebear/core';
 	import { adventurerNeutral } from '@dicebear/collection';
 	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
 	import { debounce } from 'lodash-es';
 
@@ -14,6 +13,16 @@
 	$: profileData = {
 		username: user.email?.split('@')[0],
 		imageSeed: user.name!
+	};
+
+	let usernameStatus: {
+		checking: boolean;
+		available: boolean | null;
+		error: null | string;
+	} = {
+		checking: false,
+		available: true,
+		error: null
 	};
 
 	function generateAvatar(seed: string) {
@@ -26,6 +35,9 @@
 		debouncedCheckUsernameAvailability.cancel();
 
 		if (profileData.username?.trim() === '') {
+			usernameStatus.checking = false;
+			usernameStatus.available = false;
+			usernameStatus.error = 'Username must not be empty';
 			return;
 		}
 	}
@@ -39,7 +51,7 @@
 <div class="flex-center min-h-screen min-w-full max-md:items-start">
 	<!-- card wrap -->
 	<div
-		class="relative flex min-w-[400px] flex-col rounded-lg border px-8 py-4 shadow transition hover:shadow-md max-md:min-h-screen max-md:w-full max-md:min-w-[none]"
+		class="relative flex min-w-[400px] flex-col rounded-lg border border-gray-300 px-8 py-4 shadow transition hover:shadow-md max-md:min-h-screen max-md:w-full max-md:min-w-[none]"
 	>
 		<!-- header -->
 		<h1 class="flex gap-2 text-lg font-bold">
@@ -74,13 +86,43 @@
 		<div class="">
 			<div class="grid w-full items-center gap-1">
 				<Label for="username">Username</Label>
-				<Input
-					type="text"
-					id="username"
-					bind:value={profileData.username}
-					placeholder="bob_the_builder"
-					on:input={debouncedCheckUsernameAvailability}
-				/>
+				<div
+					class="flex rounded border px-3"
+					class:border-red-600={usernameStatus.available === false && !usernameStatus.checking}
+					class:border-green-600={usernameStatus.available === true && !usernameStatus.checking}
+				>
+					<div class="flex-center">
+						<AtSign class="text-muted-foreground" size={17} />
+					</div>
+					<input
+						type="text"
+						id="username"
+						class="px-3 py-2 focus-visible:outline-none"
+						bind:value={profileData.username}
+						placeholder="bob_the_builder"
+						on:input={debouncedCheckUsernameAvailability}
+					/>
+				</div>
+				{#if usernameStatus.checking}
+					<div class="flex items-center gap-2 text-sm text-muted-foreground">
+						<Triangle class="animate-spin" size={15} />
+						<p>Checking availability</p>
+					</div>
+				{:else if usernameStatus.available === false}
+					<div class="flex items-center gap-2 text-sm text-red-700">
+						<AlertTriangle size={15} />
+						{#if usernameStatus.error !== null}
+							<p>{usernameStatus.error}</p>
+						{:else}
+							<p>Username is not available</p>
+						{/if}
+					</div>
+				{:else if usernameStatus.available === true}
+					<div class="flex items-center gap-2 text-sm text-green-700">
+						<BadgeCheck size={15} />
+						<p>Username is available</p>
+					</div>
+				{/if}
 			</div>
 		</div>
 	</div>
