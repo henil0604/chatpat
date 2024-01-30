79a2cb7b53b5275c677c09ea6e2f57290320efe7
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-30 15:52:47 +0530

    components that resolves provided src string

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import { generateAvatar, resolveURL } from '$lib/utils/avatar';
+
+	export let src: string | null | undefined;
+	let className = '';
+	export { className as class };
+	export let alt = '';
+</script>
+
+<img class={cn('', className)} {alt} src={resolveURL(src || '')} />
