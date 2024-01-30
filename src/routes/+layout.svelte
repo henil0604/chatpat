ceb1583bcc36852f6bafc0a4b2c6f6153e2eaa7f
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-31 00:57:29 +0530

    import "ldrs" package & support for new loading state datatype

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,27 @@
 	import GlobalLoading from '$lib/components/GlobalLoading.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher, mode } from 'mode-watcher';
+	import { onMount } from 'svelte';
+	import DesktopOnly from '$lib/components/DesktopOnly.svelte';
+	import MobileOnly from '$lib/components/MobileOnly.svelte';
+
+	onMount(() => {
+		import('ldrs/ring');
+	});
 </script>
 
-{#if $loading === true}
-	<GlobalLoading />
+{#if $loading.show === true}
+	{#if $loading.device === 'desktop'}
+		<DesktopOnly>
+			<GlobalLoading />
+		</DesktopOnly>
+	{:else if $loading.device === 'mobile'}
+		<MobileOnly>
+			<GlobalLoading />
+		</MobileOnly>
+	{:else}
+		<GlobalLoading />
+	{/if}
 {/if}
 
 <ModeWatcher defaultMode={'light'} />
