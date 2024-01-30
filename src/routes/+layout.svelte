f06dc5f46be03864c504b6bee13c07abc5777518
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-31 02:04:21 +0530

    calling "injectSpeedInsights" functions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,8 +12,10 @@
 	import MobileOnly from '$lib/components/MobileOnly.svelte';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	onMount(() => {
 		import('ldrs/ring');
