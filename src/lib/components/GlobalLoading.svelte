cd08fe58202fa5db7e390292d9b835a940dd5983
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-31 00:56:32 +0530

    using ring loader "ldrs" package
    previously, AnimatedChatpatTitle component was being used

--- src/lib/components/GlobalLoading.svelte
+++ src/lib/components/GlobalLoading.svelte
@@ -1,7 +1,13 @@
 <script>
-	import AnimatedChatpatTitle from '$lib/components/AnimatedChatpatTitle.svelte';
+	import { loading } from '$lib/store/global';
 </script>
 
-<div class="flex-center absolute left-0 top-0 z-[100] h-full w-full bg-background text-foreground">
-	<AnimatedChatpatTitle class="gap-4" size={40} />
+<div
+	class="flex-center absolute left-0 top-0 z-[100] h-full w-full flex-col bg-background text-foreground"
+>
+	<l-ring size="40" stroke="5" bg-opacity="0" speed="2" color="black"></l-ring>
+	{#if $loading.label && $loading.label.trim() !== ''}
+		<div class="my-2"></div>
+		<p>{$loading.label}</p>
+	{/if}
 </div>
