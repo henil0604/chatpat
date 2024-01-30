c6c211b4461c7712f23eb81344d57be47bc8fcd2
Henil Malaviya <henilmalaviya06@gmail.com>
2024-01-31 01:40:18 +0530

    debug: logging environment variables

--- scripts/pre-deployment-vercel-check.sh
+++ scripts/pre-deployment-vercel-check.sh
@@ -6,6 +6,8 @@ allowed_branches=("main" "dev")
 # Get the current branch name
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
+env
+
 # Check if the current branch is in the allowed branches array
 if [[ ! " ${allowed_branches[@]} " =~ " $current_branch " ]]; then
   echo "Error: Deployment allowed only on specific branches. Current branch: $current_branch"
