049422c5d25a1472dc4f9a1489f44ee3ee2b1472
Henil Malaviya <henilmalaviya06@gmail.com>
2024-02-08 19:28:29 +0530

    added new response code

--- src/lib/const/index.ts
+++ src/lib/const/index.ts
@@ -29,7 +29,7 @@ export const Pages: {
 	}
 };
 
-export const ResponseCodes = ['INVALID_INPUT', 'DONE', 'EMPTY_INPUT', 'ERROR', 'DATABASE_QUERY_ERROR'] as const;
+export const ResponseCodes = ['INVALID_INPUT', 'DONE', 'EMPTY_INPUT', 'ERROR', 'DATABASE_QUERY_ERROR', 'FORBIDDEN'] as const;
 
 export const DefaultTRPCResponseSchema = z.object({
 	error: z.boolean(),
