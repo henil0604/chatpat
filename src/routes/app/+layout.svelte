d13598c05a5389018e9674e9c11b01c0cd371e2a
Henil Malaviya <henilmalaviya06@gmail.com>
2024-02-09 19:46:04 +0530

    basic layout for listing contacts

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -5,10 +5,10 @@
 <div class="flex-center flex max-h-screen min-h-screen min-w-full max-w-full">
 	<!-- mobile screen wrapper -->
 	<div
-		class="h-[90dvh] w-[430px] overflow-hidden rounded border border-gray-400 shadow max-md:min-h-screen max-md:min-w-full max-md:rounded-none max-md:border-none"
+		class="relative z-[1] h-[90dvh] w-[430px] overflow-hidden rounded border border-gray-400 shadow max-md:min-h-screen max-md:min-w-full max-md:rounded-none max-md:border-none"
 	>
 		<!-- inside wrapper -->
-		<div class="relative max-h-full min-h-full min-w-full overflow-auto">
+		<div class="relative h-full min-w-full">
 			<!-- page content -->
 			<slot />
 		</div>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,15 +1,31 @@
 <script>
 	import { page } from '$app/stores';
+	import { Skeleton } from '$lib/components/ui/skeleton';
 </script>
 
-<div class="flex min-h-full min-w-full flex-col">
+<div class="relative h-full min-w-full">
 	<!-- top bar -->
-	<div class="flex w-full justify-between bg-black px-5 py-3 text-white">
+	<div class="flex w-full justify-between bg-black px-5 py-4 text-white">
 		<!-- head -->
 		<div class="text-xl">ChatPat</div>
 		<!-- tail -->
-		<div>
-			<!--  -->
-		</div>
+		<div></div>
+	</div>
+	<div class="fancy-scrollbar relative h-full min-w-full overflow-y-auto">
+		{#each Array(30) as _, i}
+			<div class="flex flex-row gap-3 px-5 py-4">
+				<div>
+					<Skeleton class="h-12 w-12 rounded-full bg-gray-300" />
+				</div>
+				<div class="flex flex-col gap-1">
+					<div>
+						<Skeleton class="h-[25px] w-[100px] bg-gray-300" />
+					</div>
+					<div>
+						<Skeleton class="h-[25px] w-[300px] bg-gray-300" />
+					</div>
+				</div>
+			</div>
+		{/each}
 	</div>
 </div>
