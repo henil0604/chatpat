324ef7958fc880995d658f9bd2bdedfdf5cf244d
Henil Malaviya <henilmalaviya06@gmail.com>
2024-02-10 22:20:16 +0530

    working footer

--- src/routes/app/Footer.svelte
+++ src/routes/app/Footer.svelte
@@ -1,7 +1,34 @@
+<script>
+	import { page } from '$app/stores';
+	import { MessageCircle, Users } from 'lucide-svelte';
+
+	const paths = [
+		{
+			href: '/app',
+			name: 'Chats',
+			icon: MessageCircle
+		},
+		{
+			href: '/app/friends',
+			name: 'Friends',
+			icon: Users
+		}
+	];
+</script>
+
 <div class="sticky bottom-0 left-0 min-w-full bg-black py-3 text-white">
-	<div class="[&>div]:flex-center flex w-full justify-between [&>div]:flex-grow">
-		<div class="">1</div>
-		<div class="">2</div>
-		<div class="">3</div>
+	<div class="[&>a]:flex-center flex w-full justify-between [&>a]:flex-grow">
+		{#each paths as path}
+			{@const isActive = $page.url.pathname === path.href}
+			<a href={path.href} class="relative {isActive ? '[&>svg]:fill-white' : ''}">
+				{#if isActive}
+					<div
+						class="absolute left-1/2 top-full h-10 w-10 -translate-x-1/2 rounded-full bg-white blur-lg"
+					></div>
+				{/if}
+
+				<svelte:component this={path.icon} />
+			</a>
+		{/each}
 	</div>
 </div>
---
+++ src/routes/app/friends/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Button } from '$lib/components/ui/button';
+	import { Skeleton } from '$lib/components/ui/skeleton';
+	import type { PageData } from './$types';
+	import { Plus } from 'lucide-svelte';
+	import Header from '../Header.svelte';
+	import Footer from '../Footer.svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="relative flex h-full min-w-full flex-col">
+	<!-- top bar -->
+	<Header />
+	<div class="flex-grow">hi friends!</div>
+	<Footer />
+</div>
