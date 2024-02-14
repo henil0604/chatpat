da4ac27d9a5b22e8cf74e3a8157052b710fd4964
Henil Malaviya <henilmalaviya06@gmail.com>
2024-02-14 15:28:45 +0530

    added notification boolean store

--- src/lib/store/app.ts
+++ src/lib/store/app.ts
@@ -1,3 +1,5 @@
 import { writable } from "svelte/store";
 
 export let isFindFriendsDrawerOpen = writable(false);
+
+export let isNotificationsDrawerOpen = writable(false);
--- src/routes/app/Header.svelte
+++ src/routes/app/Header.svelte
@@ -1,6 +1,21 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { isNotificationsDrawerOpen } from '$lib/store/app';
+	import { Bell } from 'lucide-svelte';
+</script>
+
 <div class="flex h-fit w-full justify-between bg-black px-5 py-4 text-white">
 	<!-- head -->
 	<div class="text-xl">ChatPat</div>
 	<!-- tail -->
-	<div></div>
+	<div class="flex gap-1">
+		<Button
+			variant="ghost"
+			size="sm"
+			on:click={() => ($isNotificationsDrawerOpen = true)}
+			class="h-fit w-fit rounded-full p-1"
+		>
+			<Bell size={20} />
+		</Button>
+	</div>
 </div>
